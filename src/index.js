9936a043f11d272c59d26f1903e6bdfef2d5ea23
qiang.fang <qiang.fang@huilianyi.com>
2023-02-04 15:07:04 +0800

    finish hello work

---
+++ src/index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+const { program } = require('commander');
+
+program
+  .name('helios')
+  .description('CLI to Helios JavaScript string utilities')
+  .version('0.0.1');
+
+program
+  .option('--i18n <items>' ,'对特定文件进行调整')
+  
+program.parse();
+const options = program.opts();
+console.log(options.i18n);
