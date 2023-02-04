ede55ce52c1380c5692d641e668e86221a5a8f3d
qiang.fang <qiang.fang@huilianyi.com>
2023-02-04 18:12:50 +0800

    add branchName

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const { program } = require('commander');
+const branchName = require('current-git-branch');
 
 program
   .name('helios')
@@ -8,7 +9,8 @@ program
 
 program
   .option('--i18n <items>' ,'对特定文件进行调整')
-  
+
+console.log(branchName())  
 program.parse();
 const options = program.opts();
 console.log(options.i18n);
